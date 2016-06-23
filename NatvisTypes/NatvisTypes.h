d8ee49a9ce7c3c7452fec34d5c6797377a284d6d
Sebastian Solnica <sebastian.solnica@arkena.com>
2016-06-23 15:13:33 +0200

    Updating notes

--- NatvisTypes/NatvisTypes.h
+++ NatvisTypes/NatvisTypes.h
@@ -1,2 +1,14 @@
 #pragma once
 
+#include <stdint.h>
+
+struct T0 { };
+struct T1 { };
+struct T2 { };
+struct T3 { };
+struct T4 { };
+struct T5 { };
+struct T6 { };
+struct T7 { };
+struct T8 { };
+struct T9 { };
