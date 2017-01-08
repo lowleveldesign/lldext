5a0667b22ab3139a19b48103dd5129f997e0481c
Sebastian Solnica <ssolnica@gmail.com>
2017-01-08 17:25:05 +0100

    updated version

--- LLD/DbgExts.h
+++ LLD/DbgExts.h
@@ -7,7 +7,7 @@
 #include <shlwapi.h>
 
 #define EXT_MAJOR_VER	1
-#define EXT_MINOR_VER	1
+#define EXT_MINOR_VER	2
 
 
 struct Exception
--- LLD/Shared.cpp
+++ LLD/Shared.cpp
@@ -11,10 +11,11 @@ HRESULT CALLBACK help(IDebugClient* pDebugClient, PCSTR args) {
     UNREFERENCED_PARAMETER(args);
     if (SUCCEEDED(pDebugClient->QueryInterface(__uuidof(IDebugControl), (void **)&pDebugControl))) {
         pDebugControl->Output(DEBUG_OUTPUT_NORMAL, "==============================================================\n");
-        pDebugControl->Output(DEBUG_OUTPUT_NORMAL, "  LLD v1.1 - Copyright 2016 Sebastian Solnica\n");
+        pDebugControl->Output(DEBUG_OUTPUT_NORMAL, "  LLD v1.2 - Copyright 2017 Sebastian Solnica\n");
         pDebugControl->Output(DEBUG_OUTPUT_NORMAL, "==============================================================\n\n");
-        pDebugControl->Output(DEBUG_OUTPUT_NORMAL, "  !help                - shows this help view\n\n");
-        pDebugControl->Output(DEBUG_OUTPUT_NORMAL, "  !injectdll <path>    - injects DLL into the debugee\n\n");
+        pDebugControl->Output(DEBUG_OUTPUT_NORMAL, "  !help                     - shows this help view\n\n");
+        pDebugControl->Output(DEBUG_OUTPUT_NORMAL, "  !injectdll <path>         - injects DLL into the debugee\n\n");
+        pDebugControl->Output(DEBUG_OUTPUT_NORMAL, "  !ufgraph <code-address>   - runs ufgraph on a given function (please check the documentation for requirements)\n\n");
         pDebugControl->Output(DEBUG_OUTPUT_NORMAL, "==============================================================\n\n");
 
         pDebugControl->Release();
